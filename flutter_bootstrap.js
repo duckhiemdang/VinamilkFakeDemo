280db4a3e0d0eed8d3029a633727d4458fb6ce51
duckhiem2003 <duckhiem2003@gmail.com>
2025-09-08 16:51:15 +0700

    add logging internet speed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2797955185"
+    serviceWorkerVersion: "3774099732"
   }
 });
