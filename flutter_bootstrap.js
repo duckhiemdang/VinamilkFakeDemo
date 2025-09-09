7f13163d6ebc4016b51d944a36ea8238d12beec7
duckhiem2003 <duckhiem2003@gmail.com>
2025-09-09 10:12:17 +0700

    remove internet checking component

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1589068636"
+    serviceWorkerVersion: "2871197994"
   }
 });
